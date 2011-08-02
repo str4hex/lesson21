7cd768b2c417b26fe3daa42381a364e2ef8e7f5d
srt3for <evdookimov@mail.ru>
2011-08-03 00:40:56 +0400

    +++

--- main.py
+++ main.py
@@ -1,15 +1,44 @@
 from abc import ABC, abstractmethod
 
-class Storage:
 
-    def __init__(self,items, capacity ):
-        self.items = []
+class Storage(ABC):
+
+    def __init__(self, items, capacity):
+        self.items = items
         self.capacity = capacity
 
-    def add(self):
+    @abstractmethod
+    def add(self, items, capacity):
         pass
 
-    def remove(self):
+    @abstractmethod
+    def remove(self, items, capacity):
+        pass
+
+    @abstractmethod
+    def get_free_space(self):
+        pass
+
+    @abstractmethod
+    def get_items(self):
+        pass
+
+    @abstractmethod
+    def get_unique_items_count(self):
+        pass
+
+
+class Store(Storage):
+
+    def __init__(self, items, capacity):
+        super().__init__(items, capacity)
+        self._items = {}
+        self._capacity = 100
+
+    def add(self, items, capacity):
+        pass
+
+    def remove(self, items, capacity):
         pass
 
     def get_free_space(self):
@@ -21,3 +50,25 @@ class Storage:
     def get_unique_items_count(self):
         pass
 
+
+class Shop(Storage):
+
+    def __init__(self, items, capacity):
+        super().__init__(items, capacity)
+        self._items = {}
+        self._capacity = 20
+
+    def add(self, items, capacity):
+        pass
+
+    def remove(self, items, capacity):
+        pass
+
+    def get_free_space(self):
+        pass
+
+    def get_items(self):
+        pass
+
+    def get_unique_items_count(self):
+        pass
