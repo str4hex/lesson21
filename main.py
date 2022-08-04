7268b0aabe0e082713320b803a6482e628df2e9f
srt3for <evdookimov@mail.ru>
2022-08-04 21:56:13 +0300

    +fix for items() list()

--- main.py
+++ main.py
@@ -31,7 +31,6 @@ class Store(Storage):
     items = {}
 
     def add(self, title, count):
-
         if title in self.items:
             self.items[title] += count
         else:
@@ -43,9 +42,9 @@ class Store(Storage):
         if title in self.items:
             qnt = self.items[title] - count
         if qnt <= 0:
-            self.items[title] = 0
-            self.items.pop(title)
-            pass
+            for key, value in list(self.items.items()):
+                if key == title:
+                    del store_items[key]
         else:
             self.items[title] = qnt
         self.capacity -= count
@@ -79,12 +78,26 @@ class Request:
 
 
 def main():
+    global fromm
+    global to
+    global amount
+    global product
+
     while True:
         print("\n")
         user_input = input(
             "Введите текст в формате:<действие> <количество> <наименование товара> из <место откуда> в <место куда>,\n"
-            "Например: Доставить 1 Икра-Заморская из склад в магазин\n").split(" ")
+            "Например: Доставить 10 Зефир из склад в магазин\n").split(" ")
         print("\n")
+        try:
+            fromm = user_input[4]
+            to = user_input[6]
+            amount = int(user_input[1])
+            product = user_input[2]
+        except Exception as e:
+            print(e)
+            print("Ошибка ввода текста, попробуйте пожалуйста снова,"
+                  "Например: Доставить 10 Зефир из склад в магазин")
 
         if user_input == "stop":
             break
@@ -92,36 +105,41 @@ def main():
         if len(user_input) != 7:
             print('Введен не верный запрос')
         else:
-            if user_input[4].lower() == 'склад' and user_input[2] in store.items:
-                if user_input[2] in store.items:
-                    for keys, values in store.items.items():
-                        if keys == user_input[2]:
-                            if int(user_input[1]) <= int(values):
-                                print('Нужное количество есть на складе')
-                                print(
-                                    f'Курьер везет {user_input[1]} {user_input[2]} со {user_input[4]} в {user_input[6]}')
-                                shop.add(user_input[2], int(user_input[1]))
-                                print(store.items)
-                                store.remove(user_input[2], int(user_input[1]))
-                                print(store.items)
-                                print(f'Курьер доставил {user_input[1]} {user_input[2]} в {user_input[6]}')
-                                print('В склад хранится:')
-                                print(store.get_items)
-                                print(f'Свободное место на складе {store.capacity} из 100')
-                                print('В магазин хранится:')
-                                print(shop.get_items)
-                                print(f'Свободное место в магазине {shop.capacity} из 20')
-                            else:
-                                print('Не хватает на складе, попробуйте заказать меньше')
-                                print("\n")
+            if fromm.lower() == 'склад':
+                if amount > shop.capacity:
+                    print('Нет свободного места на кладе')
                 else:
-                    print("Данного товара нет на складе")
+                    if product in store.items:
+                        for keys, values in list(store.items.items()):
+                            if keys == product:
+                                if amount <= int(values):
+                                    print('Нужное количество есть на складе')
+                                    print(f'Курьер везет {amount} {product} со {fromm} в {to}')
+                                    shop.add(product, amount)
+                                    print(store.items)
+                                    store.remove(product, amount)
+                                    print(store.items)
+                                    print(f'Курьер доставил {amount} {product} в {to}')
+                                    print('В склад хранится:')
+                                    print(store.get_items)
+                                    print(f'Свободное место на складе {store.capacity} из 100')
+                                    print('В магазин хранится:')
+                                    print(shop.get_items)
+                                    print(f'Свободное место в магазине {shop.capacity} из 20')
+                                    print(store.get_unique_items_count)
+                                    print(shop.get_unique_items_count)
+                                else:
+                                    print('Не хватает на складе, попробуйте заказать меньше')
+                                    print("\n")
+                    else:
+                        print("Данного товара нет на складе")
 
-            elif user_input[4].lower() == 'магазин':
+            elif to.lower() == 'магазин':
                 print("перевозка из магазина на склад в разработке")
 
             else:
                 print("Такой перевозки у нас нет")
+
 
 if __name__ == "__main__":
     store = Store()
@@ -131,9 +149,17 @@ if __name__ == "__main__":
         'Зефир': 10,
         "Кокос": 20,
         "Иван-чай": 7,
-        "Репа":80,
-        "Картошка":190,
-        "Икра-Заморская":1
+        "Репа": 80,
+        "Картошка": 190,
+        "Икра-Заморская": 1
     }
     store.items = store_items
     main()
+
+
+# Доставить 9 Зефир из склад в магазин
+# Доставить 3 Кокос из склад в магазин
+# Доставить 3 Иван-чай из склад в магазин
+# Доставить 3 Репа из склад в магазин
+# Доставить 3 Картошка из склад в магазин
+# Доставить 3 Икра-Заморская из склад в магазин
